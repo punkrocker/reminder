c5f9bbfbedb6dc6377a4bc75a6a38b6736fbf9b8
punkrocker <punkrock_2000@163.com>
2019-10-27 22:32:48 +0800

    删除菜单

--- src/main.ts
+++ src/main.ts
@@ -15,9 +15,7 @@ function createWindow() {
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, "../index.html"));
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools();
+  mainWindow.removeMenu();
 
   // Emitted when the window is closed.
   mainWindow.on("closed", () => {
