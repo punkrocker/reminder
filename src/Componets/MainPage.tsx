1ddb77b1273341615e6f932dfc1b887fb640a605
punkrocker <punkrock_2000@163.com>
2019-11-19 18:08:15 +0800

    html 通过 renderer 加载

---
+++ src/Componets/MainPage.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import {Component} from "react";
+
+export default class MainPage extends Component<any> {
+    public render() {
+        return <h1>hello world</h1>;
+    }
+}
