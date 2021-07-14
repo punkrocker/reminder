2c19e61ff39c99b1237bac88c9293e5685f2521e
yuhaokui <yuhaokui@healai.com>
2021-07-14 16:26:45 +0800

    简单设计界面

--- src/Componets/MainPage.tsx
+++ src/Componets/MainPage.tsx
@@ -1,7 +1,7 @@
 import * as notifier from "node-notifier";
 import * as React from "react";
 import {
-    WiredButton,
+    WiredButton, WiredListbox, WiredCheckbox
 } from "react-wired-element";
 
 // tslint:disable-next-line:interface-name
@@ -13,10 +13,29 @@ export interface MainPageProps {
 export class MainPage extends React.Component<MainPageProps, {}> {
 
     public render() {
-        return (<div onClick={this.warning.bind(this)}><WiredButton elevation={3}>Hello</WiredButton></div>);
+        return (<div>
+            <WiredListbox>
+                <WiredCheckbox text="9:00" />
+                <WiredCheckbox text="9:30" />
+                <WiredCheckbox text="10:00" />
+                <WiredCheckbox text="10:30" />
+                <WiredCheckbox text="11:00" />
+                <WiredCheckbox text="11:30" />
+                <WiredCheckbox text="12:00" />
+                <WiredCheckbox text="13:30" />
+                <WiredCheckbox text="14:00" />
+                <WiredCheckbox text="14:30" />
+                <WiredCheckbox text="15:00" />
+                <WiredCheckbox text="15:30" />
+                <WiredCheckbox text="16:00" />
+                <WiredCheckbox text="16:30" />
+                <WiredCheckbox text="17:00" />
+                <WiredCheckbox text="17:30" />
+            </WiredListbox>
+        </div>);
     }
 
     public warning() {
-        notifier.notify({title: "hello", message: "world"});
+        notifier.notify({ title: "hello", message: "world" });
     }
 }
